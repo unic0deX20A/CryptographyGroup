04b8c7084b5061f6e8e6d86bd72235c02a95152d
Dosxx <dosakouoete@gmail.com>
2020-02-20 12:47:36 -0600

    updated help function

--- GraphicalInterface.py
+++ GraphicalInterface.py
@@ -1,3 +1,7 @@
+# ICS483 Group Project
+# Authors: Kekeli D Akouete, Vang Uni A
+# Implementing encryption in an application
+
 from tkinter import filedialog
 from tkinter import *
 from tkinter import messagebox
@@ -9,7 +13,13 @@ def saveAs():
 
 def openfile():
     myFile = filedialog.askopenfilename()
-    selection = Label(root, text=myFile).pack()
+    Label(root, text=myFile).pack()
+
+
+def showhelp():
+    with open("help.txt", "r") as helper:
+        msg = helper.read()
+    messagebox.showinfo(title="About", message=msg)
     return
 
 
@@ -21,20 +31,27 @@ class Window(Frame):
         self.master.config(menu=menu)
 
         fileMenu = Menu(menu)
+        menu.add_cascade(label="File", menu=fileMenu)
         fileMenu.add_command(label="Open", command=openfile)
         fileMenu.add_command(label="Save As", command=saveAs)
         fileMenu.add_command(label="Create File")
         fileMenu.add_command(label="Exit", command=quitApp)
-        menu.add_cascade(label="File", menu=fileMenu)
 
         actionMenu = Menu(menu)
+        menu.add_cascade(label="Action", menu=actionMenu)
         actionMenu.add_command(label="Decrypt")
         actionMenu.add_command(label="Encrypt")
         actionMenu.add_command(label="Generate key")
-        menu.add_cascade(label="Action", menu=actionMenu)
+
+        helpMenu = Menu(menu)
+        menu.add_cascade(label="Help", menu=helpMenu)
+        helpMenu.add_command(label="About", command=showhelp)
 
         displayArea = Label(self.master, text="DISPLAY", justify=LEFT)
         displayArea.pack()
+
+        status = Label(self.master, text="Cryptographer_1.0 \u00AE All rights reserved", bd=1, relief=SUNKEN, anchor=W)
+        status.pack(side=BOTTOM, fill=X)
 
 
 def quitApp():
--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from cryptography.fernet import Fernet
-import tkinter
 import os
 
 
